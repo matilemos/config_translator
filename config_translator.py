36a923a8d95b1d9f03d0c2fb92bde7925bd2095e
matilemos <matias.d.lemos@gmail.com>
2017-07-18 15:52:54 -0300

    estructura interfaces e instancias

--- config_translator.py
+++ config_translator.py
@@ -4,6 +4,7 @@
 
 import sys
 import json
+import xml.etree.ElementTree as ET
 import codecs
 import jinja2
 from pprint import pprint
@@ -25,9 +26,12 @@ vpns = {}
 ifaces_list = {'all': [],'pending': [], 'sco': [],'irs': [], 'vrf': [],'vpls': [],'bridge': [], 'trunk':[]}
 instances_list = {'vrf': [], 'vpls': [], 'bridge':[], 'noassigned':[]}
 
-templateLoader = jinja2.FileSystemLoader( searchpath="/home/mlemos/github/config_translator/templates" )
+templateLoader = jinja2.FileSystemLoader( searchpath="templates" )
 templateEnv = jinja2.Environment( loader=templateLoader )
 
+
+rac_config = {'interfaces':[], 'routing-instances':[]}
+sac_config = {'interfaces':[]}
 
 def main():
     
@@ -60,6 +64,7 @@ def main():
 
     # Armado listado de interfaces e instancias a migrar:
 
+
     for interface in nodoc['interfaces'][0]['interface']: # Recorro las interfaz del Nodo C que corresponden a SCOs
         if(interface['name']['data']) in ifaces_list['sco']:  # Selecciono unicamente las que vamos a migrar
             if_name=interface['name']['data']
@@ -68,26 +73,41 @@ def main():
                 ifaces_list['all'].append(interface_name)
                 ifaces_list['pending'].append(interface_name)
 
-                if ('encapsulation' in unit.keys() and 
-                    unit['encapsulation'][0]['data'] == 'vlan-vpls'):  # Identifico VPLS
-                   
-                   ifaces_list['vpls'].append(interface_name)
-                   ifaces_list['pending'].remove(interface_name)
+                for element in sco:  #Cargo SCO
+                    if sco[element]['interface'] == if_name:
+                        data = {'sco':sco[element]['vlan']}
 
-                   for instance in nodoc['routing-instances'][0]['instance']:
+                if ('description' in unit.keys()): #Cargo descripcion
+                    data['description']=unit['description'][0]['data']
+                else:
+                    data['description']= "Sin descripcion"
+                                    
+                if ('encapsulation' in unit.keys() and 
+                    unit['encapsulation'][0]['data'] == 'vlan-vpls'):  # Identifico servicio VPLS
+                   
+                    ifaces_list['vpls'].append(interface_name)
+                    ifaces_list['pending'].remove(interface_name)
+
+                    data['service']='vpls'
+
+                    if ('vlan-id' in unit.keys()):  # Identifico vlan
+                        data['vlan']=unit['vlan-id'][0]['data']
+                    
+                    for instance in nodoc['routing-instances'][0]['instance']:
                         if ('interface' in instance.keys()):
                             for iface in instance['interface']:
                                 if (interface_name == iface['name']['data']):
                                     if (instance['name']['data'] not in instances_list['vpls']):
                                         instances_list['vpls'].append(instance['name']['data'])
                                         instances_list['noassigned'].append(instance['name']['data'])
-                                        
+                                        data['instance']=instance['name']['data']
 
                 if ('encapsulation' in unit.keys() and 
                     unit['encapsulation'][0]['data'] == 'vlan-bridge'):  # Identifico bridges
                    
                     ifaces_list['bridge'].append(interface_name)
                     ifaces_list['pending'].remove(interface_name)
+                    data['service']='bridge'
 
                     for domain in nodoc['bridge-domains'][0]['domain']:
                         if ('interface' in domain.keys()):
@@ -95,15 +115,22 @@ def main():
                                 if (interface_name == iface['name']['data']):
                                     if (domain['name']['data'] not in instances_list['bridge']):
                                         instances_list['bridge'].append(domain['name']['data'])
+                                        data['instance']=domain['name']['data']
 
                     
                 if ('family' in unit.keys() and 
                     'inet' in unit['family'][0].keys() and 
-                    'address' in unit['family'][0]['inet'][0].keys()):
+                    'address' in unit['family'][0]['inet'][0].keys()):  # Identifico IRS
                     
                     ifaces_list['irs'].append(interface_name)
                     ifaces_list['pending'].remove(interface_name)
+                    data['service']='irs'
+                    data['ip']=unit['family'][0]['inet'][0]['address'][0]['name']['data']
 
+                    if ('vlan-id' in unit.keys()):  # Identifico vlan
+                        data['vlan']=unit['vlan-id'][0]['data']
+
+                    
                     for instance in nodoc['routing-instances'][0]['instance']:
                         if ('interface' in instance.keys()):
                             for iface in instance['interface']:
@@ -114,6 +141,9 @@ def main():
                                     if (instance['name']['data'] not in instances_list['vrf']):
                                         instances_list['vrf'].append(instance['name']['data'])
                                         instances_list['noassigned'].append(instance['name']['data'])
+                                        data['service']='vrf'
+                                        data['instance']=instance['name']['data']
+                
                 
                 if ('family' in unit.keys() and 
                     'bridge' in unit['family'][0].keys() and 
@@ -122,51 +152,88 @@ def main():
                     
                     ifaces_list['trunk'].append(interface_name)
                     ifaces_list['pending'].remove(interface_name)
+                    data['service']='trunk'
     
+                rac_config['interfaces'].append(data)
 
-    # Identificacion de VPLS-id
-
-    for vpls in instances_list['vpls']:
-        for instance in nodoc['routing-instances'][0]['instance']:
-            if (vpls == instance['name']['data']):
-                id_cv = instance['protocols'][0]['vpls'][0]['vpls-id'][0]['data']
-                for vpn in vpns:
-                    if id_cv == vpns[vpn]['id_cv']:
-                        vpns[vpn]['alias'] = vpls
-                        if vpls in instances_list['noassigned']:
-                            instances_list['noassigned'].remove(vpls)
-
+    '''
     # Identificacion de VRF-id
+    
     for vrf in instances_list['vrf']:
         for instance in nodoc['routing-instances'][0]['instance']:
-            if (vrf == instance['name']['data']):
-                if ('vrf-target' in instance and 'vrf-import' in instance):
-                    id_cv = instance['vrf-target'][0]['community'][0]['data'].split(":")[1] + ":" + instance['vrf-target'][0]['community'][0]['data'].split(":")[2]
-                    for vpn in vpns:
-                        if id_cv == vpns[vpn]['id_cv']:
-                            vpns[vpn]['alias'] = vrf
-                            if vrf in instances_list['noassigned']:
-                                instances_list['noassigned'].remove(vrf)
 
-                if ('vrf-import' in instance):
-                    vrf_imp = instance['vrf-import'][0]['data']
-                    for policy in nodoc['policy-options'][0]['policy-statement']:
-                        if instance['vrf-import'][0]['data'] == policy['name']['data']:
-                            for term in policy['term']:
-                                if ('from' in term and
-                                    'community' in term['from'][0]):
-                                    for community in term['from'][0]['community']:
-                                        if community['data'] != 'GESTION' and community['data'] != 'Gestion-FC-Import':
-                                            for item in nodoc['policy-options'][0]['community']:
-                                                if community['data'] == item['name']['data']:
-                                                    for member in item['members']:
-                                                        id_cv = member['data'].split(":")[1] + ":" + member['data'].split(":")[2]
-                                                        for vpn in vpns:
-                                                            if id_cv == vpns[vpn]['id_cv']:
-                                                                vpns[vpn]['alias'] = vrf
-                                                                if vrf in instances_list['noassigned']:
-                                                                    instances_list['noassigned'].remove(vrf)
+
+
                 
+
+    '''
+    for instance in nodoc['routing-instances'][0]['instance']:
+        if (instance['name']['data'] in instances_list['vpls']):
+            data = {'name': instance['name']['data']}
+            
+            if ('description' in instance.keys()):
+                data['description'] = instance['description'][0]['data']
+
+            if ('interface' in instance.keys()):
+                data['interfaces']=[]
+                for interface in instance['interface']:
+                    data['interfaces'].append(interface['name']['data'])
+
+            if ('protocols' in instance.keys() and
+                'vpls' in instance['protocols'][0].keys() and
+                'vpls-id' in instance['protocols'][0]['vpls'][0].keys()):
+                data['id_cv'] = instance['protocols'][0]['vpls'][0]['vpls-id'][0]['data']
+                for vpn in vpns:
+                    if (data['id_cv'] == vpns[vpn]['id_cv']):
+                        vpns[vpn]['alias'] = data['name']
+                        data['id_fc'] = vpns[vpn]['id_fc']
+                        if data['name'] in instances_list['noassigned']:
+                            instances_list['noassigned'].remove(data['name'])
+                                        
+
+        if (instance['name']['data'] in instances_list['vrf']):
+            data = {'name': instance['name']['data']}
+            
+            if ('description' in instance.keys()):
+                data['description'] = instance['description'][0]['data']
+            
+            if ('interface' in instance.keys()):
+                data['interfaces']=[]
+                for interface in instance['interface']:
+                    data['interfaces'].append(interface['name']['data'])            
+
+            if ('vrf-target' in instance and 'vrf-import' not in instance):
+                data['id_cv'] = instance['vrf-target'][0]['community'][0]['data'].split(":")[1] + ":" + instance['vrf-target'][0]['community'][0]['data'].split(":")[2]
+                for vpn in vpns:
+                    if data['id_cv'] == vpns[vpn]['id_cv']:
+                        vpns[vpn]['alias'] = data['name']
+                        if data['name'] in instances_list['noassigned']:
+                             instances_list['noassigned'].remove(data['name'])
+
+            if ('vrf-import' in instance):
+                for policy in nodoc['policy-options'][0]['policy-statement']:
+                    if instance['vrf-import'][0]['data'] == policy['name']['data']:
+                        for term in policy['term']:
+                            if ('from' in term and 
+                                'community' in term['from'][0]):
+                                for community in term['from'][0]['community']:
+                                    if community['data'] != 'GESTION' and community['data'] != 'Gestion-FC-Import':
+                                        for item in nodoc['policy-options'][0]['community']:
+                                            if community['data'] == item['name']['data']:
+                                                for member in item['members']:
+                                                    data['id_cv'] = member['data'].split(":")[1] + ":" + member['data'].split(":")[2]
+                                                    for vpn in vpns:
+                                                        if data['id_cv'] == vpns[vpn]['id_cv']:
+                                                            vpns[vpn]['alias'] = data['name']
+                                                            data['id_fc'] = vpns[vpn]['id_fc']
+                                                            if data['name'] in instances_list['noassigned']:
+                                                                instances_list['noassigned'].remove(data['name'])
+
+            rac_config['routing-instances'].append(data)
+
+    #pprint(rac_config)
+
+
 
     # Verififacion de asignacion de VPNS:
     if instances_list['noassigned']:
@@ -176,21 +243,13 @@ def main():
         print("\n\nCompletar y volver a ejecutar.\n")
         return
 
+
     for file in listdir("output"): # Borro archivos en carpeta "output"
         remove("output/" + file)
 
-    for interface in ifaces_list['irs']:
-    #    pprint(interface)
-        for iface in nodoc['interfaces'][0]:
-            
-            
-            break
-    template = templateEnv.get_template('irs_interface.j2')
-    irs = {'sco': '123', 'vlan': '13412','ip_a': '1.1.1.1', 'ip_b': '2.2.2.2'}
-    test=template.render(irs)
 
-    with open('output/test.txt', 'a') as f:
-        f.write(test)
+
+
 
 if __name__ == '__main__':
         exit(main())
